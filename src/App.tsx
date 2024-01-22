2700dd20b97be376d32a640bd08561c668d850cb
Shadman-Shakib-27 <s.s.a.27.8.1999@gmail.com>
2024-01-22 23:13:54 +0600

    Some Part Added.

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Lesson1 from "./Pages/Lesson1";
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <Lesson1 />
+    </div>
+  );
+}
+
+export default App;
